a9ed2c2ed8ff0160479bcd7f47125cd8f1d89355
zhuchengcheng <zhuchengcheng@netease.corp.com>
2019-11-20 15:29:05 +0800

    "init flutter"

---
+++ .ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/cc/Desktop/programing/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/cc/Desktop/programing/flutterboot_test/zcctest"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
